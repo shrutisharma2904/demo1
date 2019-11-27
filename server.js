8e242078852d893a07e826b2af9d4d1485162ef5
Shruti <shruti@gmail.com>
2019-11-27 14:33:31 +0530

    my first git

---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require("express")
+var app = express();
+app.listen(1234,()=>{
+    console.log("server started..");
+})
+app.get("/",()=>{
+    console.log("heyy");
+})
+app.get("/",()=>{
+    console.log("heyy second time");
+})
